a76dbeeaeda8644f5266dae049525acc4a47025c
RaguRamanTB <aakash1956ragu@gmail.com>
2019-12-07 18:42:17 +0530

    File Picker for Upload

--- app/src/main/java/com/example/ipfs_android/Create.java
+++ app/src/main/java/com/example/ipfs_android/Create.java
@@ -1,14 +1,73 @@
 package com.example.ipfs_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+
+import java.util.regex.Pattern;
 
 public class Create extends AppCompatActivity {
 
+    static Button browse;
+    static TextView file;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1001);
+        }
+
+        file = findViewById(R.id.createFile);
+        browse = findViewById(R.id.browse);
+
+        browse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialFilePicker()
+                        .withActivity(Create.this)
+                        .withRequestCode(1000)
+                        .withFilter(Pattern.compile(".*\\.txt$"))
+                        .withHiddenFiles(true)
+                        .start();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000 && resultCode == RESULT_OK) {
+            String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+            file.setText(filePath);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1001:{
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission Granted!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Permission Denied!", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
     }
 }
